57a4dca1d4652f0bc130596a16567d1b89f0abe1
Cristiane Silva <cristianeavelarmaia@gmail.com>
2018-06-06 20:43:18 +0000

    Count Letters function working

--- characterCounting.js
+++ characterCounting.js
@@ -1,7 +1,22 @@
-function countLetters () {
- // return unique characters
+function countLetters(string) {
+  var noSpaces = string.split(" ").join("");
+  //1. Creates an empty object
+  var stats = {};
+  //Every time the loop runs it checks it that key already exists, if it does it just increments the count
+  for (var i = 0; i < noSpaces.length; i++){
+    var currentLetter = noSpaces[i];
+    //If currentLetter does not exist, it creates the property
+    if (!stats[currentLetter]) {
+          stats[currentLetter] = 0;
+        }
+        //Increments instance
+      stats[currentLetter]++
+      }
+ console.log(stats);
+ }
+
  //how many instances of each letter
  //return an object where each unique character is a key and number of instances is the value
-}
 
-countLetters("lighthouse in the house")
+
+countLetters("lighthouse in the house");
