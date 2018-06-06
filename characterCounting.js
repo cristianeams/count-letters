29b393273008d32204a838bc1746d26dfe783019
Cristiane Silva <cristianeavelarmaia@gmail.com>
2018-06-06 18:36:56 +0000

    First Commit

---
+++ characterCounting.js
@@ -0,0 +1,7 @@
+function countLetters () {
+ // return unique characters
+ //how many instances of each letter
+ //return an object where each unique character is a key and number of instances is the value
+}
+
+countLetters("lighthouse in the house")
